148f5f593b8685f86be2511e79ed817c890d0810
VIVEK <vivek@ubuntu.ubuntu-domain>
2021-01-04 13:19:55 +0530

    iChat version 1

---
+++ server.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+const {userJoin,getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+const botName = 'iChat BOT';
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connexts
+io.on('connection', socket => {
+    // console.log('New Web Socket connection...');
+
+    socket.on('joinRoom', ({ username, room }) => {
+        const user = userJoin(socket.id, username, room);
+        
+        socket.join(user.room);
+
+        // Welcome current user
+        socket.emit('message', formatMessage(botName, 'Welcome to iChat'));
+
+        // Broadcast when a user connects
+        socket.broadcast
+            .to(user.room)
+            .emit(
+                'message',
+                formatMessage(botName, `${user.username} has joined the chat`)
+        );
+
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+    });
+    
+    // Runs when client disconects
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', formatMessage(botName, `${user.username} has left the chat`));
+
+            // Send users and room info
+            io.to(user.room).emit('roomUsers', {
+                room: user.room,
+                users: getRoomUsers(user.room)
+            });
+
+        }
+
+    });
+    // message to all the clients
+    // io.emit();
+    
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        // console.log(msg);
+
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg, user.id));
+    });
+});
+
+const PORT = 5500 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on ${PORT}`));
